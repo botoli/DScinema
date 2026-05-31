fe29697f9df62e9104ac69fd481e02cff21ae926
botoli <standrey2007@gmail.com>
2026-06-01 00:32:42 +0300

    qwq

--- app/components/Roulette.tsx
+++ app/components/Roulette.tsx
@@ -111,10 +111,10 @@ function Roulette({ films }: RouletteProps) {
         // Обновляем отображаемые фильмы
         setCurrentFilms([...remainingFilmsRef.current]);
         setCurrentHighlight(null);
-      }, 50);
+      }, 500);
 
       timeoutRefs.current.push(timeoutId);
-    }, 100);
+    }, 250);
   };
 
   // Остановка рулетки
