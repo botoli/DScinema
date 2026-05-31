7876bbbdb8c26cb414985c4a1646377f72bb4578
botoli <standrey2007@gmail.com>
2026-05-31 22:31:00 +0300

    add icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import "./globals.css";
-
+export const metadata = {
+  title: "Ds Cinema",
+  icons: {
+    icon: "/favicon.png", // Next.js сам найдет файл в app/
+  },
+};
 export default function RootLayout({
   children,
 }: Readonly<{
